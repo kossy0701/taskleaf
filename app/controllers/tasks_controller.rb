10b32a92f90e00667d9de699dc089ee9b4b02698
kossy0701 <0421kossy0421@gmail.com>
2018-11-18 11:03:05 +0900

    add 「」

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   def create
     task = Task.new(task_params)
     task.save!
-    redirect_to tasks_path, notice: "タスク#{task.name}を登録しました。"
+    redirect_to tasks_path, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   def show
