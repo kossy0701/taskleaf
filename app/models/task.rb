9b8ec2ee12a8578d359c81645215261953897426
kossy0701 <0421kossy0421@gmail.com>
2018-11-19 04:10:00 +0900

    add validation not include comma

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true
+  validate :validate_name_not_include_comma
+
+  private
+
+  def validate_name_not_include_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
