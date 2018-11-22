32f8b5d23424c4ea4ebcc2523ebffb9c529c549f
kossy0701 <0421kossy0421@gmail.com>
2018-11-22 14:29:39 +0900

    add parameters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,6 +54,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :descriptio, :image)
   end
 end
